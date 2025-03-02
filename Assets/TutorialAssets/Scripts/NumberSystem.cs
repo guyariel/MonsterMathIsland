8814c2ac0825a0da59d68ae902788ed9228e0e8c
Juttacci <guy.hazoume@epitech.eu>
2025-03-02 17:56:50 +0100

    ci: Complete the number system script

--- Assets/TutorialAssets/Scripts/NumberSystem.cs
+++ Assets/TutorialAssets/Scripts/NumberSystem.cs
@@ -1,11 +1,28 @@
+using TMPro;
 using UnityEngine;
 
 public class NumberSystem : MonoBehaviour
 {
+    [SerializeField] TMP_Text _messageTextField;
+    string _messageText;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // Find two random numbers
+        int firstNumber = Random.Range(1,50);
+        int secondNumber = Random.Range(1,50);
+
+        if (firstNumber < secondNumber)
+        {
+            _messageText = $"{firstNumber} is lower than {secondNumber}";
+        }
+        else
+        {
+            _messageText = $"{firstNumber} is higher than {secondNumber}";
+        }
+
+        _messageTextField.text = _messageText;
     }
 
     // Update is called once per frame
