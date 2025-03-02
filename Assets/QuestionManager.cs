061499cd02ae8d98546e47629f3e9c6ca6fa1e19
Juttacci <guy.hazoume@epitech.eu>
2025-03-02 17:24:23 +0100

    ci: Add an if clause operation system (addition or subtraction))

--- Assets/QuestionManager.cs
+++ Assets/QuestionManager.cs
@@ -6,19 +6,34 @@ public class QuestionManager : MonoBehaviour
 {
 
     [SerializeField] TMP_Text _messageBoxTextField;
+    [SerializeField] TMP_InputField _answerInputField;
+
+    string question;
+    [SerializeField] int answer;
  
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        /*
+        Find random values for the operation
+        */
         int operand1 = UnityEngine.Random.Range(1,100); 
         int operand2 = UnityEngine.Random.Range(1,100);
         
-        // Log operand
-        Debug.Log(operand1);
-        Debug.Log(operand2);
+        if (UnityEngine.Random.value < 0.5f)
+        {
+            question = $"{operand1} + {operand2} = ";
+            answer = operand1 + operand2;
+        }
+        else
+        {
+            question = $"{operand1} - {operand2} = ";
+            answer = operand1 - operand2;
+        }
 
+        _messageBoxTextField.text = question;
 
-        _messageBoxTextField.text = "Hello world, nice to meet you";
+        _answerInputField.Select();
     }
 
     // Update is called once per frame
