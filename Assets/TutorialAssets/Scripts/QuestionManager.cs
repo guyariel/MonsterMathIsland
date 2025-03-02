9d5e192d77f254052004cd0ee7783517e11b9132
Juttacci <guy.hazoume@epitech.eu>
2025-03-02 18:32:56 +0100

    ci: Create the validateQuestion and clearInputField methods

--- Assets/TutorialAssets/Scripts/QuestionManager.cs
+++ Assets/TutorialAssets/Scripts/QuestionManager.cs
@@ -14,12 +14,18 @@ public class QuestionManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        generateQuestions();
+    }
+
+    void generateQuestions ()
+    {
         /*
         Find random values for the operation
         */
         int operand1 = UnityEngine.Random.Range(1,100); 
         int operand2 = UnityEngine.Random.Range(1,100);
         
+        // Addition or Subtraction
         if (UnityEngine.Random.value < 0.5f)
         {
             question = $"{operand1} + {operand2} = ";
@@ -33,7 +39,25 @@ public class QuestionManager : MonoBehaviour
 
         _messageBoxTextField.text = question;
 
-        _answerInputField.Select();
+        clearInputField();
+    }
+
+    public void validateAnswer()
+    {
+        if (_answerInputField.text == answer.ToString())
+        {
+            generateQuestions();
+        }
+        else 
+        {
+            clearInputField();
+        }
+    }
+
+    void clearInputField()
+    {
+        _answerInputField.text = "";
+        _answerInputField.ActivateInputField();
     }
 
     // Update is called once per frame
